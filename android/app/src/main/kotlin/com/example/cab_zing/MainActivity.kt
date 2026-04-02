05034267cefdac8445774aecea60764cfee25300
roshan <aashiqroshan@gmail.com>
2026-04-02 18:49:20 +0530

    [Feat] initial commit

---
+++ android/app/src/main/kotlin/com/example/cab_zing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cab_zing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:cab_zing/pages/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWindow());
+}
+
+class AppWindow extends StatelessWidget {
+  const AppWindow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SplashScreen());
+  }
+}
